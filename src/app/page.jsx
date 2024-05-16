f3738cb872b52fcb9dd61c4611fa07a254ddc911
GVillaf <gonzaloalejandro720@gmail.com>
2024-05-15 23:14:05 -0300

    fix(-): escape unescaped entities in Home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <main className="bg-cover bg-center text-yellow-400 min-h-screen flex flex-col items-center justify-between p-8 md:max-h-screen overflow-y-hidden relative w-full md:h-screen">
       <div className="absolute inset-0 z-[-10]">
-        <Image src={image1} alt="Star Wars Background" fill style={{ objectFit: 'cover' }}  className="opacity-60" />
+        <Image src={image1} alt="Star Wars Background" fill style={{ objectFit: 'cover' }} className="opacity-60" />
         <div className="absolute inset-0 bg-black opacity-30"></div>
       </div>
       <div className="flex flex-col items-center mt-12">
         <h1 className="md:text-6xl text-5xl font-bold text-center">WookieeWiki</h1>
-        <p className="text-2xl text-center mt-4 font-bold">"Explore the Star Wars Galaxy"</p>
+        <p className="text-2xl text-center mt-4 font-bold">&quot;Explore the Star Wars Galaxy&quot;</p>
       </div>
       <div className="flex flex-wrap justify-center gap-8 mb-12">
         <Link href="/films">
